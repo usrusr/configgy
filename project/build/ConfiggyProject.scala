f6afe0e542819d1987b66de657b101383fae192e
Robey Pointer <robeypointer@gmail.com>
2010-04-20 17:13:22 -0700

    StandardProject.

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -1,19 +1,7 @@
 import sbt._
 
-class OstrichProject(info: ProjectInfo) extends DefaultProject(info) {
-  // Maven repositories
-  val scalaToolsTesting = "testing.scala-tools.org" at "http://scala-tools.org/repo-releases/"
-  val powerMock = "powermock-api" at "http://powermock.googlecode.com/svn/repo/"
-  val mavenDotOrg = "repo1" at "http://repo1.maven.org/maven2/"
-  val scalaToolsReleases = "scala-tools.org" at "http://scala-tools.org/repo-releases/"
-  val reucon = "reucon" at "http://maven.reucon.com/public/"
-  val lagDotNet = "lag.net" at "http://www.lag.net/repo/"
-  val oauthDotNet = "oauth.net" at "http://oauth.googlecode.com/svn/code/maven"
-  val javaDotNet = "download.java.net" at "http://download.java.net/maven/2/"
-  val jBoss = "jboss-repo" at "http://repository.jboss.org/maven2/"
-  val nest = "nest" at "http://www.lag.net/nest/"
-  val atlassian = "atlassian" at "https://m2proxy.atlassian.com/repository/public/"
 
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1"
+class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) {
+  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
 }

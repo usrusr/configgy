ce55a752fbda89be209bbb0b7dd6fa12dd16f14a
Ismael Juma <ismael@juma.me.uk>
2010-05-10 20:59:06 +0100

    Update to Scala 2.8.0.RC2.

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -2,6 +2,6 @@ import sbt._
 
 
 class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) {
-  val specs = "org.scala-tools.testing" % "specs_2.8.0.RC1" % "1.6.5-SNAPSHOT" % "test"
+  val specs = "org.scala-tools.testing" % "specs_2.8.0.RC2" % "1.6.5-SNAPSHOT" % "test"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
 }
